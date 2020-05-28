567902881bd9c3c3e27e174ade01f7324a3c9fb3
VUTIEN <vutienbka@gmail.com>
2020-05-28 21:01:38 +0700

    Flappy bird game

---
+++ JavaScriptFiles/base.js
@@ -0,0 +1,43 @@
+// va background
+
+let Base= function (game) {
+    this.game= game; // de truy xuat context trong frame "game"
+    this.loaded= false;
+    this.baseX =0;
+    this.baseWidth = 336;
+    this.baseHeight= 112;
+
+    let self= this;
+
+    this.init= function(){
+        this.loadImage();
+    };
+
+    this.loadImage= function(){
+
+        this.image= new Image();  // ham Image() la ham co san trong javascript
+        this.image.onload= function () {
+            self.loaded= true;
+           // console.log('image loaded');
+        };
+        self.image.src= 'Images/base.png';
+    };
+
+    this.update= function () {
+        //console.log('Update background');
+        if(this.game.gameOver)
+            return;
+        this.baseX-=3;
+        if(this.baseX== -this.baseWidth)
+            this.baseX= 0;
+    };
+
+    this.draw= function () {
+        if(this.loaded==false)
+            return;
+        self.game.context.drawImage(this.image,this.baseX, this.game.height - this.baseHeight);
+        self.game.context.drawImage(this.image,this.baseX + this.game.width, this.game.height - this.baseHeight);
+    };
+
+
+};
